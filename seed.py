368b2d2f02a1fe6af16c6c602de08467ebcaadde
nerov103 <nibir01000@gmail.com>
2024-02-11 21:25:48 +0600

    seed method in python random library

--- seed.py
+++ seed.py
@@ -1,29 +1,26 @@
 import random
 
-random.seed(10)
-print(random.random())
 
+#example 1
 
+# for i in range(10):
+#     random.seed(0)
+#     print(random.randint(1, 100))
 
+#example 2
 
+# random.seed(3)
+# print(random.randint(1, 100))
 
+# random.seed(3)
+# print(random.randint(1, 100))
 
+# print(random.randint(1, 100))
 
+#example 3
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+random.seed(6)
+print(random.randint(1, 10))
 
 
 
